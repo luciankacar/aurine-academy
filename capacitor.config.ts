a140a385a25f001efcddb3641cc58dc5af7a1d91
lucja <lucja.carpenter@interia.pl>
2026-01-07 09:23:32 +0100

    add android platform

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.80834e947d144ddf8de56b4c449882ed',
-  appName: 'aurine-academy',
+  appId: 'com.aurine.academy',
+  appName: 'Aurine Academy',
   webDir: 'dist',
   server: {
     url: 'https://80834e94-7d14-4ddf-8de5-6b4c449882ed.lovableproject.com?forceHideBadge=true',
