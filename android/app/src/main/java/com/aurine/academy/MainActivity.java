4d65db74336c7e4a62f42b94866c60f35756ffac
lucja <lucja.carpenter@interia.pl>
2026-01-07 10:31:02 +0100

    fix: Android icons and fullscreen MainActivity

--- android/app/src/main/java/com/aurine/academy/MainActivity.java
+++ android/app/src/main/java/com/aurine/academy/MainActivity.java
@@ -1,5 +1,46 @@
 package com.aurine.academy;
 
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowInsets;
+import android.view.WindowInsetsController;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Możesz tu zarejestrować pluginy Capacitor, jeśli masz
+        // this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
+        //     // add(YourPlugin.class);
+        // }});
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        enableFullscreen();
+    }
+
+    private void enableFullscreen() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            // Android 11+
+            getWindow().setDecorFitsSystemWindows(false);
+            WindowInsetsController controller = getWindow().getInsetsController();
+            if (controller != null) {
+                controller.hide(WindowInsets.Type.navigationBars() | WindowInsets.Type.statusBars());
+                controller.setSystemBarsBehavior(WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
+            }
+        } else {
+            // Android 10 i starsze
+            getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                View.SYSTEM_UI_FLAG_FULLSCREEN |
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+            );
+        }
+    }
+}
